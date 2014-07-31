165fa2395311ea040803bdd82602f5b9fa4b6f11
timbits07 <timbits@me>
2014-07-31 16:14:36 -0700

    like button working

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var app = angular.module("meLikeyApp", ["firebase"]);
 
 app.controller('postsCtrl', function($scope, $firebase){
 	var postsRef = new Firebase('https://me-likey.firebaseio.com/');
-	$scope.posts = $firebase(postsRef);
+	var sync = $firebase(postsRef);
+	$scope.posts = sync.$asArray();
 	$scope.addPost = function(title, image, link, desc, cat) {
-		console.log(title);
-		$scope.posts.$add({title:title, image:image, url:link, blurb:desc, category:cat});
+		$scope.posts.$add({title: title, image: image, url: link, blurb: desc, category: cat, liked:false, likeImage:"img/me_likey.png"});
 		$scope.newPostTitle = "";
 		$scope.newImageUrl = "";
 		$scope.newLinkUrl = "";
@@ -16,14 +16,24 @@ app.controller('postsCtrl', function($scope, $firebase){
 
 
 	$scope.deletePost = function(post){
-		console.log(post.$id)
-			var check = confirm("Are you sure you want to delete this post?");
-			if (check == true){
-			var postRef = new Firebase("https://me-likey.firebaseio.com/" + post.$id);
-			postRef.remove();
+		var check = confirm("Are you sure you want to delete this post?");
+		if (check == true){
+		$scope.posts.$remove(post);
+
 		}
 	};
+
+	$scope.likePost = function(post){
+		var likeRef = new Firebase("https://me-likey.firebaseio.com/" + post.$id);
+		likeRef.update({ liked: true });
+	}
+
+	$scope.unlikePost = function(post){
+		var likeRef = new Firebase("https://me-likey.firebaseio.com/" + post.$id);
+		likeRef.update({ liked: false });
+	}
 });
 
 
-// postsRef.$remove(post.$id);
+// var postRef = new Firebase("https://me-likey.firebaseio.com/" + post.$id);
+// 	postRef.remove();
