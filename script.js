1bc1564f288c62adecf689c5d76e4b6accaa314d
timbits07 <timbits@me>
2014-07-31 11:42:13 -0700

    commit before changing over to most current version of firebase

--- script.js
+++ script.js
@@ -3,25 +3,27 @@ var app = angular.module("meLikeyApp", ["firebase"]);
 app.controller('postsCtrl', function($scope, $firebase){
 	var postsRef = new Firebase('https://me-likey.firebaseio.com/');
 	$scope.posts = $firebase(postsRef);
-	$scope.addPost = function() {
-		$scope.posts.$add({"title":$scope.newPostTitle, "image":$scope.newImageUrl, "url":$scope.newLinkUrl, "blurb":$scope.newPostDesc, "category":$scope.newPostCat});
+	$scope.addPost = function(title, image, link, desc, cat) {
+		console.log(title);
+		$scope.posts.$add({title:title, image:image, url:link, blurb:desc, category:cat});
 		$scope.newPostTitle = "";
 		$scope.newImageUrl = "";
 		$scope.newLinkUrl = "";
 		$scope.newPostDesc = "";
 		$scope.newPostCat = "";
 		$('#myModal').modal('hide');
-	};	
+	};
 
-	$scope.delete = function(post){
-		console.log(post);
-		var check = confirm("Are you sure you want to delete this post?");
-		if (check == true) {
-			post.remove();
+
+	$scope.deletePost = function(post){
+		console.log(post.$id)
+			var check = confirm("Are you sure you want to delete this post?");
+			if (check == true){
+			var postRef = new Firebase("https://me-likey.firebaseio.com/" + post.$id);
+			postRef.remove();
 		}
-	}
+	};
 });
 
 
-
-
+// postsRef.$remove(post.$id);
